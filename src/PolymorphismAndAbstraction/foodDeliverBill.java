273e5ec76b66e5b142d368683c5df1fac8300fcf
Abhaygupta1101 <144575642+Abhaygupta1101@users.noreply.github.com>
2026-09-07 07:26:48 +0530

    n

--- src/PolymorphismAndAbstraction/foodDeliverBill.java
+++ src/PolymorphismAndAbstraction/foodDeliverBill.java
@@ -2,4 +2,12 @@ package PolymorphismAndAbstraction;
 
 public class foodDeliverBill {
 
+//	double calculateBill(double foodPrice) {
+//		return foodPrice;
+//	}
+//	double calculateBill(double foodPrice, int quantity) {
+//		return foodPrice*quantity;
+//	}
+//	double calculateBill(double foodBill)
+//	}
 }
